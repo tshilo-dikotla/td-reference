8d2314f2386aaa63fbdf34dcbd0ac9f105dbe4b9
tumiemosweu <imosweuk@gmail.com>
2019-01-05 21:16:11 +0200

    add infant reference config

--- td_reference/reference_model_configs.py
+++ td_reference/reference_model_configs.py
@@ -6,10 +6,16 @@ site_reference_configs.register_from_visit_schedule(
             'td_maternal.maternalvisit', 'td_infant.infantvisit']})
 
 configs = {
-    'td_maternal.maternalultrasoundinitial': ['number_of_gestations'],
+    'td_maternal.maternalultrasoundinitial': ['number_of_gestations', 'edd_confirmed'],
     'td_maternal.maternalcontraception': ['srh_referral'],
-    'td_maternal.maternalpostpartumdep': ['maternal_visit'],
+    'td_maternal.rapidtestresult': ['result_date'],
+    'td_maternal.maternalpostpartumdep': ['laugh'],
     'td_infant.infantdeathreport': ['death_date'],
+    'td_infant.infantnvpdispensing': ['nvp_prophylaxis'],
+    #     'td_infant.infantarvprophmod': ['dose_status'],
+    'td_infant.infantfu': ['has_dx', 'physical_assessment'],
+    'td_infant.infantfeeding': ['formula_intro_occur'],
+    'td_infant.infantbirthdata': ['congenital_anomalities']
 
 }
 
