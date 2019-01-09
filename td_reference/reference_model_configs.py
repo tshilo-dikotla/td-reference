98d2c15a7ad951923ec416f062e06027a420164c
Coulson Kgathi <ckgathi@gmail.com>
2019-01-09 11:44:51 +0200

    add infant visit reference range with appointment model from td-infant

--- td_reference/reference_model_configs.py
+++ td_reference/reference_model_configs.py
@@ -2,8 +2,8 @@ from edc_reference import site_reference_configs
 
 site_reference_configs.register_from_visit_schedule(
     visit_models={
-        'edc_appointment.appointment': [
-            'td_maternal.maternalvisit', 'td_infant.infantvisit']})
+        'edc_appointment.appointment': ['td_maternal.maternalvisit'],
+        'td_infant.appointment': ['td_infant.infantvisit']})
 
 configs = {
     'td_maternal.maternalultrasoundinitial': ['number_of_gestations'],
