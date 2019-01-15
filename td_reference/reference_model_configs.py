95ca141e707686c28f1782e22c0e74755bdc7768
tumiemosweu <imosweuk@gmail.com>
2019-01-15 11:58:07 +0200

    update reference config

--- td_reference/reference_model_configs.py
+++ td_reference/reference_model_configs.py
@@ -2,10 +2,11 @@ from edc_reference import site_reference_configs
 
 site_reference_configs.register_from_visit_schedule(
     visit_models={
-        'edc_appointment.appointment': [
-            'td_maternal.maternalvisit', 'td_infant.infantvisit']})
+        'edc_appointment.appointment': ['td_maternal.maternalvisit'],
+        'td_infant.appointment': ['td_infant.infantvisit']})
 
 configs = {
+    'td_maternal.maternalrando': ['rx'],
     'td_maternal.maternalultrasoundinitial': ['number_of_gestations', 'edd_confirmed'],
     'td_maternal.maternalcontraception': ['srh_referral'],
     'td_maternal.rapidtestresult': ['result_date'],
@@ -13,7 +14,6 @@ configs = {
     'td_infant.infantdeathreport': ['death_date'],
     'td_infant.infantnvpdispensing': ['nvp_prophylaxis'],
     'td_infant.infantarvproph': ['arv_status'],
-    #     'td_infant.infantarvprophmod': ['dose_status'],
     'td_infant.infantfu': ['has_dx', 'physical_assessment'],
     'td_infant.infantfeeding': ['formula_intro_occur'],
     'td_infant.infantbirthdata': ['congenital_anomalities']
