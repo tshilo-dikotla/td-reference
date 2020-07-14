30a17f2c738d8cce64e76fef37a8a63c44d23328
tumiemosweu <imosweuk@gmail.com>
2020-07-14 14:31:28 +0200

    bump versio

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='td-reference',
-    version='0.1.3',
+    version='0.1.4',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
